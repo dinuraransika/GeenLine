fc20fa190eac306911ef724373f488d70a0abc70
Dinura  Ransika <dinuraransika@192.168.1.2>
2019-10-03 09:31:33 +0530

    1st commit

---
+++ GeenLineWebApp/src/java/Model/Passenger.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+import java.util.Date;
+
+/**
+ *
+ * @author dinuraransika
+ */
+public class Passenger {
+    
+    int registeredPassengerNumber;
+    String email,password,title,fName,lName,gender,nationality,DOB,phoneNumber,addressLine1,addressLine2,addressLine3,town,countryOfResident;
+    Date dateOfRegistration;
+    
+}
